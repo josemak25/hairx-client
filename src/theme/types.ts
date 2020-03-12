0fa55911807f2157b96984e23d329ea6d117927b
josemak25 <amakirij@gmail.com>
2020-03-12 17:05:58 +0100

    add change to app theme

--- src/theme/types.ts
+++ src/theme/types.ts
@@ -2,10 +2,10 @@ import 'styled-components';
 
 // All app colors
 enum COLORS {
-  BD_DARK_COLOR = '#00B1BB',
+  BD_DARK_COLOR = '#F4F8FB',
   BG_LIGHT_COLOR = '#FFFFFF',
-  FONT_DARK_COLOR = '#333333',
-  FONT_MILK_COLOR = '#F5F5F5'
+  FONT_DARK_COLOR = '#0D0E10',
+  FONT_LIGHT_COLOR = '#68696F'
 }
 
 // All app font sizes
@@ -13,8 +13,8 @@ enum FONTS {
   SMALL_SIZE = 10,
   MEDIUM_SIZE = 12,
   LARGE_SIZE = 15,
-  FAMILY_REGULAR = 'notosans-regular',
-  FAMILY_BOLD = 'notosans-bold',
+  NOTOSANS_REGULAR = 'notosans-regular',
+  NOTOSANS_BOLD = 'notosans-bold',
   FONT_WEIGHT_LIGHT = 200,
   FONT_WEIGHT_MEDIUM = 600,
   FONT_WEIGHT_HEAVY = 800
@@ -27,7 +27,7 @@ declare module 'styled-components' {
       BD_DARK_COLOR: string;
       BG_LIGHT_COLOR: string;
       FONT_DARK_COLOR: string;
-      FONT_MILK_COLOR: string;
+      FONT_LIGHT_COLOR: string;
     };
 
     // All Global App Font Sizes
@@ -35,8 +35,8 @@ declare module 'styled-components' {
       SMALL_SIZE: number;
       MEDIUM_SIZE: number;
       LARGE_SIZE: number;
-      FAMILY_REGULAR: string;
-      FAMILY_BOLD: string;
+      NOTOSANS_REGULAR: string;
+      NOTOSANS_BOLD: string;
       FONT_WEIGHT_LIGHT: number;
       FONT_WEIGHT_MEDIUM: number;
       FONT_WEIGHT_HEAVY: number;
