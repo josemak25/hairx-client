8559b5257ec4afc3dfb6ad475d85899f5d80e757
josemak25 <amakirij@gmail.com>
2020-03-12 17:33:14 +0100

    add splash component

--- src/screens/splash/index.tsx
+++ src/screens/splash/index.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
-
 import Button from '../../components/button';
+import { NavigationInterface } from '../types';
+
 import { Container, Welcome } from './styles';
 
-export default function Splash() {
+interface SplashScreenProp extends NavigationInterface {
+  testID?: string;
+}
+
+export default function Splash(props: SplashScreenProp) {
   return (
     <Container>
       <Welcome>Splash Screen</Welcome>
