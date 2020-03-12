b1b86009912ad05e0321eeaf50594da6a69d9354
josemak25 <amakirij@gmail.com>
2020-03-12 17:32:44 +0100

    add home component

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
-
 import Button from '../../components/button';
+import { NavigationInterface } from '../types';
+
 import { Container, Welcome } from './styles';
+
+interface SplashScreenProp extends NavigationInterface {
+  testID?: string;
+}
 
 export default function Home() {
   return (
