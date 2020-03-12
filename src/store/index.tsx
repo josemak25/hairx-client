ef619e93255fe97f1b7451cba5615f343c5fb34a
josemak25 <amakirij@gmail.com>
2020-03-12 17:47:33 +0100

    add store provider for app

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,22 +1,37 @@
-import React, { createContext, useReducer, FunctionComponent } from 'react';
+import React, {
+  createContext,
+  useReducer,
+  useContext,
+  FunctionComponent
+} from 'react';
+
 import useCombinedReducers from '../utils/useCombinedReducers';
 
-import postsReducer, { postInitialState } from './posts/reducer';
 import userReducer, { userInitialState } from './user/reducer';
+import { UserInitialState } from './user/types';
 
-const StoreContext = createContext(null);
+// We define our type for the context properties right here
+export interface ContextProps {
+  state: { userState: UserInitialState };
+  dispatch?: any;
+}
+
+const StoreContext = createContext<ContextProps>({
+  state: { userState: userInitialState }
+});
 
 const StoreProvider: FunctionComponent = ({ children }) => {
   const [state, dispatch] = useCombinedReducers({
-    user: useReducer(userReducer, userInitialState),
-    posts: useReducer(postsReducer, postInitialState)
+    userState: useReducer(userReducer, userInitialState)
   });
 
   return (
-    <StoreContext.Provider value={[state, dispatch]}>
+    <StoreContext.Provider value={{ state, dispatch }}>
       {children}
     </StoreContext.Provider>
   );
 };
 
-export { StoreProvider, StoreContext };
+const useStoreContext = () => useContext(StoreContext);
+
+export { StoreProvider, useStoreContext };
