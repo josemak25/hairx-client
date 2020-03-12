dd47210e6f23224bd9cb45931ffe66463d525999
josemak25 <amakirij@gmail.com>
2020-03-12 17:07:42 +0100

    add custom constant back button style for back naviagtion on the app

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,30 @@
+// App Icons prop types
+export type IconProps = {
+  testID?: string;
+  width?: string;
+  height?: string;
+  fillColor?: string;
+  useCase?: boolean;
+  style?: object;
+};
+
 // App header custom style
 export const customHeaderStyle = {
   borderBottomWidth: 0,
   elevation: 0,
-  shadowOpacity: 0
+  shadowOpacity: 0,
+  backgroundColor: '#F4F8FB'
+};
+
+// Navigation Header back button default
+export const navigationBackButton = {
+  headerBackTitle: 'back',
+  headerBackColor: '#0D0E10',
+  headerTintColor: '#0D0E10',
+  headerBackTitleStyle: {
+    color: '#0D0E10',
+    fontFamily: 'notosans-regular',
+    textTransform: 'capitalize',
+    fontSize: 12
+  }
 };
