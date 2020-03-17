d590f312940c15a493f69fe48687a84ec4a817ac
josemak25 <amakirij@gmail.com>
2020-03-17 17:44:02 +0100

    add configurations on all store

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -8,21 +8,36 @@ import React, {
 import useCombinedReducers from '../utils/useCombinedReducers';
 
 import userReducer, { userInitialState } from './user/reducer';
+import cartReducer, { cartInitialState } from './cart/reducer';
+import productReducer, { productInitialState } from './product/reducer';
+
 import { UserInitialState } from './user/types';
+import { CartInitialState } from './cart/types';
+import { ProductInitialState } from './product/types';
 
 // We define our type for the context properties right here
 export interface ContextProps {
-  state: { userState: UserInitialState };
+  state: {
+    userState: UserInitialState;
+    cartState: CartInitialState;
+    productState: ProductInitialState;
+  };
   dispatch?: any;
 }
 
 const StoreContext = createContext<ContextProps>({
-  state: { userState: userInitialState }
+  state: {
+    userState: userInitialState,
+    cartState: cartInitialState,
+    productState: productInitialState
+  }
 });
 
 const StoreProvider: FunctionComponent = ({ children }) => {
   const [state, dispatch] = useCombinedReducers({
-    userState: useReducer(userReducer, userInitialState)
+    userState: useReducer(userReducer, userInitialState),
+    cartState: useReducer(cartReducer, cartInitialState),
+    productState: useReducer(productReducer, productInitialState)
   });
 
   return (
