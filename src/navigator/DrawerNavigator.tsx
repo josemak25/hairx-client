842ffdad989bc1f5d7d3b959524027cbd8862cae
josemak25 <amakirij@gmail.com>
2020-03-17 17:54:48 +0100

    add drawer navigator component with implementations of all screens

--- src/navigator/DrawerNavigator.tsx
+++ src/navigator/DrawerNavigator.tsx
@@ -2,17 +2,27 @@ import React from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import BottomNavigator from './BottomNavigator';
 
-import {
-  TrackOrdersStack,
-  PrivacyAndPolicyStack,
-  AboutUsStack
-} from './DrawerStack';
+import Screens from '../screens';
+import DrawerStack from './DrawerStack';
+import CustomDrawer from './CustomDrawer';
+
+import { useThemeContext } from '../theme';
+import { Dimensions } from 'react-native';
 
 const Drawer = createDrawerNavigator();
 
 export default function DrawerNavigator() {
+  const { colors } = useThemeContext();
+
   return (
-    <Drawer.Navigator initialRouteName="HomeScreen">
+    <Drawer.Navigator
+      initialRouteName="HomeScreen"
+      drawerContent={props => <CustomDrawer {...props} />}
+      drawerStyle={{
+        width: Dimensions.get('window').width - 50,
+        backgroundColor: colors.DRAWER_COLOR
+      }}
+    >
       <Drawer.Screen
         name="HomeScreen"
         component={BottomNavigator}
@@ -20,18 +30,39 @@ export default function DrawerNavigator() {
       />
       <Drawer.Screen
         name="TrackOrdersScreen"
-        component={TrackOrdersStack}
         options={{ title: 'Track Orders' }}
+        children={props => (
+          <DrawerStack
+            stackName="TrackOrdersScreen"
+            stackTitle="Track Orders"
+            component={Screens.TrackOrdersScreen}
+            {...props}
+          />
+        )}
       />
       <Drawer.Screen
         name="PrivacyAndPolicyScreen"
-        component={PrivacyAndPolicyStack}
         options={{ title: 'Privacy & Policy' }}
+        children={props => (
+          <DrawerStack
+            stackName="PrivacyAndPolicyScreen"
+            stackTitle="Privacy & Policy"
+            component={Screens.PrivacyAndPolicyScreen}
+            {...props}
+          />
+        )}
       />
       <Drawer.Screen
         name="AboutUsScreen"
-        component={AboutUsStack}
         options={{ title: 'About Us' }}
+        children={props => (
+          <DrawerStack
+            stackName="AboutUsScreen"
+            stackTitle="About Us"
+            component={Screens.AboutUsScreen}
+            {...props}
+          />
+        )}
       />
     </Drawer.Navigator>
   );
