b977325ff62d66eb00a30c4ee8fb6bcabbfac7c6
josemak25 <amakirij@gmail.com>
2020-03-18 09:23:08 +0100

    add empty cart component styles

--- src/screens/cart/styles.ts
+++ src/screens/cart/styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components/native';
+import applyScale from '../../utils/applyScale';
 
 export const Container = styled.View`
   flex: 1;
@@ -7,9 +8,36 @@ export const Container = styled.View`
   background-color: ${({ theme }) => theme.colors.BD_DARK_COLOR};
 `;
 
-export const Welcome = styled.Text`
-  font-size: ${({ theme }) => theme.fonts.LARGE_SIZE}px;
+export const EmptyCartContainer = styled.View`
+  flex: 1;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const EmptyCartTextContainer = styled.View`
+  flex: 0.7;
+  width: 100%;
+  justify-content: space-around;
+  align-items: center;
+`;
+
+export const EmptyCartTextHolder = styled.View``;
+
+export const EmptyCartTittle = styled.Text`
+  width: ${applyScale(250)}px;
+  font-size: ${({ theme }) => theme.fonts.LARGE_SIZE + 2}px;
+  font-family: ${({ theme }) => theme.fonts.NOTOSANS_BOLD};
+  color: ${({ theme }) => theme.colors.FONT_DARK_COLOR};
+  text-align: center;
+  padding: 10px;
+  text-transform: capitalize;
+`;
+
+export const EmptyCartDescription = styled.Text`
+  width: ${applyScale(270)}px;
+  font-size: ${({ theme }) => theme.fonts.MEDIUM_SIZE + 2}px;
   font-family: ${({ theme }) => theme.fonts.NOTOSANS_REGULAR};
   color: ${({ theme }) => theme.colors.FONT_DARK_COLOR};
-  text-transform: capitalize;
+  text-align: center;
 `;
