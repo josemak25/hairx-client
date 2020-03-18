ca15fc33cdfe5fcd5022c1fca5e3e6ccda2d58a5
josemak25 <amakirij@gmail.com>
2020-03-18 09:24:20 +0100

    add fix for general header style

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,20 +1,9 @@
+import { theme } from '../theme/types';
+
 // App header custom style
 export const customHeaderStyle = {
   borderBottomWidth: 0,
   elevation: 0,
   shadowOpacity: 0,
-  backgroundColor: '#F4F8FB'
-};
-
-// Navigation Header back button default
-export const navigationBackButton = {
-  headerBackTitle: 'back',
-  headerBackColor: '#0D0E10',
-  headerTintColor: '#0D0E10',
-  headerBackTitleStyle: {
-    color: '#0D0E10',
-    fontFamily: 'notosans-regular',
-    textTransform: 'capitalize',
-    fontSize: 12
-  }
+  backgroundColor: theme.colors.BD_DARK_COLOR
 };
