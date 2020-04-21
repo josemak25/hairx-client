44e3a1c8caefd42fe5199021faaca7980f6d8382
dreplica <adejo.david@aiesec.net>
2020-04-21 14:42:17 +0100

    used style guide for header font family

--- src/screens/signup/phoneVerification/style.ts
+++ src/screens/signup/phoneVerification/style.ts
@@ -3,14 +3,20 @@ import { Dimensions } from 'react-native';
 
 export const Container = styled.View`
   flex: 1;
+  margin: 0px 15px;
 `;
 
 export const Header = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.JOST_BOOK};
   width: 311px;
   font-size: 30px;
   text-align: left;
+  height: 43px;
 `;
 
 export const Subtitle = styled.Text`
   font-size: 20px;
+  text-align: left;
+  width: 327px;
+  height: 53px;
 `;
