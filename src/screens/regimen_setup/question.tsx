494ea9cd18842702526c392db1094e4823f71fc7
OreAkintobi <oreakintobi@gmail.com>
2020-04-21 19:22:08 +0100

    created question component for regimen screen

---
+++ src/screens/regimen_setup/question.tsx
@@ -0,0 +1,90 @@
+import React from 'react';
+import { FlatList } from 'react-native';
+import { useThemeContext } from '../../theme';
+import Button from '../../components/button';
+
+import {
+  Container,
+  RegimenQuestionBody,
+  QuestionContainer,
+  QuestionTitle,
+  QuestionRelevanceHeader,
+  QuestionRelevanceText,
+  AnswersContainer,
+  AnswerOption,
+  AnswerOptionText,
+  ButtonContainer
+} from './styles';
+
+interface RenderItemProp {
+  testID?: string;
+  question: string;
+  questionRelevance: string;
+  index: number;
+  options: string[];
+  handleNext(): void;
+  handlePrevious(): void;
+}
+
+export default function RenderItem(props: RenderItemProp) {
+  const { colors } = useThemeContext();
+
+  const {
+    question,
+    questionRelevance,
+    options,
+    index,
+    handleNext,
+    handlePrevious
+  } = props;
+
+  return (
+    <Container>
+      <RegimenQuestionBody>
+        <QuestionContainer>
+          <QuestionTitle>{question}</QuestionTitle>
+          <QuestionRelevanceHeader>Question Relevance</QuestionRelevanceHeader>
+          <QuestionRelevanceText>{questionRelevance}</QuestionRelevanceText>
+        </QuestionContainer>
+        <AnswersContainer>
+          <FlatList
+            data={options}
+            showsVerticalScrollIndicator={false}
+            keyExtractor={option => option}
+            renderItem={({ item }) => {
+              return (
+                <AnswerOption>
+                  <AnswerOptionText>{item}</AnswerOptionText>
+                </AnswerOption>
+              );
+            }}
+          />
+        </AnswersContainer>
+        <ButtonContainer>
+          <Button
+            title="previous"
+            buttonStyle={{
+              width: 120,
+              backgroundColor: colors.BG_WHITE_COLOR,
+              borderWidth: 1,
+              borderColor: colors.BG_WHITE_COLOR
+            }}
+            onPress={handlePrevious}
+            textStyle={{ color: colors.FONT_DARK_COLOR, opacity: 0.3 }}
+          />
+          <Button
+            title={'next'}
+            buttonStyle={{
+              width: 120,
+              backgroundColor: colors.BG_WHITE_COLOR,
+              borderWidth: 1,
+              borderColor: colors.INACTIVE_FIELD_COLOR
+            }}
+            onPress={handleNext}
+            textStyle={{ color: colors.FONT_DARK_COLOR }}
+          />
+        </ButtonContainer>
+      </RegimenQuestionBody>
+    </Container>
+  );
+}
