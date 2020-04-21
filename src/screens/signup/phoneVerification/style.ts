d162088f893a53c98138ae1e2d91b5dc2b0c6995
dreplica <adejo.david@aiesec.net>
2020-04-21 13:48:38 +0100

    phoneverification width is total screen width of the phone

--- src/screens/signup/phoneVerification/style.ts
+++ src/screens/signup/phoneVerification/style.ts
@@ -1,6 +1,12 @@
 import styled from 'styled-components/native';
+import { Dimensions } from 'react-native';
 
-export const Container = styled.View``;
+const { width: WIDTH } = Dimensions.get('window');
+
+export const Container = styled.View`
+  flex: 1;
+  width: ${WIDTH}px;
+`;
 
 export const Header = styled.Text``;
 
