96d90e73f8933af6385d5863078695dbaaa85784
Izuking <matthiasogbodo@gmail.com>
2020-04-21 17:34:57 +0100

    add  settings texts styles

--- src/screens/settings/styles.ts
+++ src/screens/settings/styles.ts
@@ -34,3 +34,25 @@ export const HeaderTitleLabel = styled.Text`
   text-transform: capitalize;
   padding: 0px 5px;
 `;
+
+export const TextContainer = styled.View`
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  top: 40px;
+`;
+
+export const AboutText = styled.Text`
+  font-size: ${({ theme }) => theme.fonts.MEDIUM_SIZE}px;
+  font-family: ${({ theme }) => theme.fonts.JOST_BOOK};
+  color: ${({ theme }) => theme.colors.INACTIVE_FIELD_COLOR};
+  text-transform: uppercase;
+  padding-bottom: 10px;
+`;
+
+export const Description = styled.Text`
+  font-size: ${({ theme }) => theme.fonts.SMALL_SIZE * 2}px;
+  font-family: ${({ theme }) => theme.fonts.CORMORANT_REGULAR};
+  color: ${({ theme }) => theme.colors.BUTTON_DARK_COLOR};
+  padding: 0px 16px;
+`;
