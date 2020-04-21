e71ca7e02479cbdfbab853ab6a3d744901a67d76
dreplica <adejo.david@aiesec.net>
2020-04-21 20:33:17 +0100

    creating a component to show the current country the user has selected

---
+++ src/screens/signup/phoneVerification/verification/countryDisplay/style.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  width: 100%;
+  height: 45px;
+  border-radius: 5px;
+  background-color: ${({ theme }) => theme.colors.INPUT_FIELD_COLOR};
+`;
+
+export const Text = styled.Text``;
