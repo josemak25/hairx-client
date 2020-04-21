4733bc95009d085164d9daed0cfa4392f8859258
dreplica <adejo.david@aiesec.net>
2020-04-22 00:46:18 +0100

    added @twotalltotems/react-native-otp-input

--- src/screens/signup/phoneVerification/verification/country.tsx
+++ src/screens/signup/phoneVerification/verification/country.tsx
@@ -3,16 +3,16 @@ import CountryPicker, {
   Country,
   CountryCode
 } from 'react-native-country-picker-modal';
-import PhoneInput from 'react-native-phone-input';
-
-import { Container, Text } from './style';
 import CountryDisplay from './countryDisplay';
 import ReactNativePhoneInput from 'react-native-phone-input';
 import { TextInput } from 'react-native';
+import PhoneInput from 'react-native-phone-input';
+
+import { Container, Text } from './style';
 import { theme } from '../../../../theme/types';
 export default function InputCountry() {
   const [country, setCountry] = useState<string>('United States');
-  const [phoneNumber, setPhoneNumber] = useState<string>('');
+  const [phoneNumber, setPhoneNumber] = useState<string>('+1');
   const [vis, setvisibility] = useState<boolean>(false);
   const [code, setCode] = useState<CountryCode>('US');
   const phoneRef = useRef<ReactNativePhoneInput<typeof TextInput>>(null);
@@ -58,7 +58,8 @@ export default function InputCountry() {
           fontFamily: theme.fonts.JOST_BOOK as string,
           opacity: true ? 0.3 : 0.7
         }}
-        // flagStyle={{ width: 0, height: 0 }}
+        onChangePhoneNumber={val => setPhoneNumber(phoneNumber + val)}
+        flagStyle={{ width: 0, height: 0 }}
         initialCountry={code.toLocaleLowerCase()}
         textProps={{
           placeholder: '202-555-0152'
