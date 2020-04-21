aa8f25fed5ecc6de81a48cde77a5bc7e4e3c3cef
dreplica <adejo.david@aiesec.net>
2020-04-21 16:27:21 +0100

    just removed react-native-phone-input

--- src/screens/signup/phoneVerification/verification/country.tsx
+++ src/screens/signup/phoneVerification/verification/country.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import PhoneInput from 'react-native-phone-input';
 
 import { Container, Info, Text } from './style';
 export default function InputCountry() {
+  const phone = useRef();
   return (
     <Container>
       {/* country select here library*/}
       <Text>What's your number?</Text>
       {/* input phone number library*/}
+      <PhoneInput ref={phone} />
     </Container>
   );
 }
