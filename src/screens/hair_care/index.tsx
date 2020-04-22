2d53909d240ca8325630bc2718df3754772e2231
Izuking <matthiasogbodo@gmail.com>
2020-04-22 12:55:43 +0100

    rf-animate the refresh button

--- src/screens/hair_care/index.tsx
+++ src/screens/hair_care/index.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Ionicons } from '@expo/vector-icons';
 import Button from '../../components/button';
 import { NavigationInterface } from '../types';
 import SafeAreaView from '../../commons/header/safe-area-view';
 import Header from '../../commons/header/header';
 import { useThemeContext } from '../../theme';
+import { Animated } from 'react-native';
 
 import {
   Container,
@@ -21,8 +22,24 @@ interface HairCareScreenScreenProp extends NavigationInterface {
 
 export default function HairCareScreen(props: HairCareScreenScreenProp) {
   const { colors } = useThemeContext();
+  const [state] = useState({ animation: new Animated.Value(0) });
 
   const handleRefresh = () => {};
+
+  const startAnimation = () => {
+    handleRefresh();
+    Animated.timing(state.animation, {
+      toValue: 360,
+      duration: 1000
+    }).start(() => {
+      state.animation.setValue(0);
+    });
+  };
+
+  const rotateInterpolation = state.animation.interpolate({
+    inputRange: [0, 360],
+    outputRange: ['0deg', '360deg']
+  });
 
   return (
     <SafeAreaView>
@@ -34,16 +51,23 @@ export default function HairCareScreen(props: HairCareScreenScreenProp) {
           </HeaderTitleContainer>
         )}
         headerRight={() => (
-          <RefreshButton
-            onPress={handleRefresh}
+          <AnimatedRefreshButton
+            onPress={startAnimation}
             underlayColor="rgba(59, 59, 59, 0.5)"
+            style={{
+              transform: [
+                {
+                  rotate: rotateInterpolation
+                }
+              ]
+            }}
           >
             <Ionicons
               name="md-refresh"
               size={25}
               color={colors.BUTTON_DARK_COLOR}
             />
-          </RefreshButton>
+          </AnimatedRefreshButton>
         )}
       />
 
@@ -54,3 +78,5 @@ export default function HairCareScreen(props: HairCareScreenScreenProp) {
     </SafeAreaView>
   );
 }
+
+const AnimatedRefreshButton = Animated.createAnimatedComponent(RefreshButton);
