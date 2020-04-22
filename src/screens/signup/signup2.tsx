ecdd5f586508df1123df1cc6e8bea5b254c0ba1c
chidiogo-akunne <chidiogoakunne@gmail.com>
2020-04-22 14:18:44 +0100

    create email input field

--- src/screens/signup/signup2.tsx
+++ src/screens/signup/signup2.tsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { SafeAreaView, TouchableOpacity } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 
 import { NavigationInterface } from '../types';
 import { useThemeContext } from '../../theme';
 
-import { Container, Welcome, Prompt, NavigationView, Pagination, OfText } from './styles';
+import Button from '../../components/button';
+import Input from '../../components/input';
+
+import {
+  Container,
+  Welcome,
+  Prompt,
+  NavigationView,
+  Pagination,
+  OfText
+} from './styles';
 
 interface SignupScreenTwoProp extends NavigationInterface {
   testID?: string;
@@ -13,20 +23,37 @@ interface SignupScreenTwoProp extends NavigationInterface {
 
 export default function SignUpScreenTwo(props: SignupScreenTwoProp) {
   const { colors, fonts } = useThemeContext();
+
+  const [email, setEmail] = useState('clairetamara08@gmail.com');
+  const [values, setValues] =  useState({
+      firstName: '',
+      lastName: ''
+  })
+
   return (
     <Container>
-     <SafeAreaView>
-         <NavigationView>
-            <TouchableOpacity onPress={() => props.navigation.goBack()}>
+      <SafeAreaView>
+        <NavigationView>
+          <TouchableOpacity onPress={() => props.navigation.goBack()}>
             <Ionicons name="ios-arrow-back" size={35} color="#707070" />
-            </TouchableOpacity>
-            <Pagination>2 
-                <OfText> of </OfText>
-                 4</Pagination>
-         </NavigationView>
-         <Welcome>Good to have you</Welcome>
-         <Prompt>CREATE ACCOUNT</Prompt>
-     </SafeAreaView>
+          </TouchableOpacity>
+          <Pagination>
+            2<OfText> of </OfText>4
+          </Pagination>
+        </NavigationView>
+        <Welcome>Good to have you</Welcome>
+        <Prompt>CREATE ACCOUNT</Prompt>
+        <Input
+          defaultValue={email}
+          onChangeText={input => setEmail(input)}
+          placeholder={email}
+          textInputStyle={{
+            height: 60,
+            fontSize: fonts.LARGE_SIZE,
+            marginBottom: '10%'
+          }}
+        />
+      </SafeAreaView>
     </Container>
   );
 }
