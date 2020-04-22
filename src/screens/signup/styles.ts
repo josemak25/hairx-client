fbf5876b622ccaa1ad0302155837d8428ce7ba35
chidiogo-akunne <chidiogoakunne@gmail.com>
2020-04-22 03:50:11 +0100

    create and style option text

--- src/screens/signup/styles.ts
+++ src/screens/signup/styles.ts
@@ -10,16 +10,21 @@ export const Welcome = styled.Text`
   font-size: ${({ theme }) => theme.fonts.LARGE_SIZE + 5}px;
   font-family: ${({ theme }) => theme.fonts.JOST_BOOK};
   color: ${({ theme }) => theme.colors.FONT_DARK_COLOR};
-  margin: 30% 0 1%
+  margin: 30% 0 1%;
 `;
 
 export const Prompt = styled.Text`
-font-size: ${({ theme }) => theme.fonts.LARGE_SIZE + 15}px;
+  font-size: ${({ theme }) => theme.fonts.LARGE_SIZE + 15}px;
   font-family: ${({ theme }) => theme.fonts.JOST_MEDIUM};
   color: ${({ theme }) => theme.colors.FONT_DARK_COLOR};
-  margin: 0 0 20%
+  margin: 0 0 15%;
 `;
 
-export const Choice = styled.Text``;
+export const Choice = styled.Text`
+  text-align: center;
+  margin: 4% 0;
+  font-size: ${({ theme }) => theme.fonts.LARGE_SIZE}px;
+  font-family: ${({ theme }) => theme.fonts.JOST_BOOK};
+`;
 
 export const Terms = styled.Text``;
