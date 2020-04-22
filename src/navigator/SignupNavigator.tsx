9f494f28a9f3d984ef30a6d2d4f6d9a790ff4a0c
dreplica <adejo.david@aiesec.net>
2020-04-22 14:40:00 +0100

    OTPVerification page is rendering correctly

--- src/navigator/SignupNavigator.tsx
+++ src/navigator/SignupNavigator.tsx
@@ -1,32 +1,23 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
-import Screens from '../screens';
+import Screens from '../screens/signup/index';
 
 const SignupStack = createStackNavigator();
 
 export default function SignupNavigator() {
   return (
     <SignupStack.Navigator initialRouteName="SignupScreen" headerMode="screen">
-      <SignupStack.Screen
-        name="SignupScreen"
-        component={Screens.SignupScreen}
-      />
-      <SignupStack.Screen
-        name="SignupScree1"
-        component={Screens.SignupScreen}
-      />
-      <SignupStack.Screen
-        name="SignupScree2"
-        component={Screens.SignupScreen}
-      />
-      <SignupStack.Screen
+      {/* <SignupStack.Screen
         name="SignupScree3"
         component={Screens.SignupScreen}
-      />
+      /> */}
 
       <SignupStack.Screen
-        name="RegimenSetupScreen"
-        component={Screens.RegimenSetupScreen}
+        name="OTPScreen"
+        component={Screens.OTPVerification}
+        options={{
+          headerShown: false
+        }}
       />
     </SignupStack.Navigator>
   );
--- src/screens/signup/index.tsx
+++ src/screens/signup/index.tsx
@@ -1,48 +1,44 @@
 import React, { useRef } from 'react';
 import { ScrollView } from 'react-native';
 import { NavigationInterface } from '../types';
+import OTPVerification from './otpVerification';
 
-import { Container } from './styles';
-import Phoneverify from './phoneVerification';
-import VerifyDetails from '../../libs/getPhoneverify.json';
+// import { Container } from './styles';
+// import Phoneverify from './phoneVerification';
+// import VerifyDetails from '../../libs/getPhoneverify.json';
 
-interface SignupScreenProp extends NavigationInterface {
-  testID?: string;
-}
-
-// import {
-//   PhoneVerification,
-//   OTPVerification
+// interface SignupScreenProp extends NavigationInterface {
+//   testID?: string;
 // }
 
-// export default {
-//   PhoneVerification,
-//   OTPVerification
+export default {
+  // PhoneVerification,
+  OTPVerification
+};
+
+// export default function SignupScreen(props: SignupScreenProp) {
+//   const scrollRef = useRef<ScrollView>(null);
+
+//   return (
+//     <Container>
+//       <ScrollView
+//         horizontal={true}
+//         showsHorizontalScrollIndicator={false}
+//         pagingEnabled={true}
+//         ref={scrollRef}
+//       >
+//         {VerifyDetails.map((item, ind) => (
+//           <Phoneverify
+//             key={ind}
+//             index={ind}
+//             scroll={scrollRef}
+//             buttonText={item.buttonText}
+//             context={item.context}
+//             header="VERIFY YOUR IDENTITY"
+//             subtitle={item.subtitle}
+//           />
+//         ))}
+//       </ScrollView>
+//     </Container>
+//   );
 // }
-
-export default function SignupScreen(props: SignupScreenProp) {
-  const scrollRef = useRef<ScrollView>(null);
-
-  return (
-    <Container>
-      <ScrollView
-        horizontal={true}
-        showsHorizontalScrollIndicator={false}
-        pagingEnabled={true}
-        ref={scrollRef}
-      >
-        {VerifyDetails.map((item, ind) => (
-          <Phoneverify
-            key={ind}
-            index={ind}
-            scroll={scrollRef}
-            buttonText={item.buttonText}
-            context={item.context}
-            header="VERIFY YOUR IDENTITY"
-            subtitle={item.subtitle}
-          />
-        ))}
-      </ScrollView>
-    </Container>
-  );
-}
