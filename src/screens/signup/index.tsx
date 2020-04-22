d903295bc4b9ae19c76d3088a8393c36ae4009e0
dreplica <adejo.david@aiesec.net>
2020-04-22 09:47:04 +0100

    imported json file to be used as verifyDetails on phoneverify index

--- src/screens/signup/index.tsx
+++ src/screens/signup/index.tsx
@@ -1,10 +1,10 @@
 import React, { useRef } from 'react';
-import Button from '../../components/button';
+import { ScrollView } from 'react-native';
 import { NavigationInterface } from '../types';
 
 import { Container, Welcome } from './styles';
 import Phoneverify from './phoneVerification';
-import { ScrollView } from 'react-native';
+import VerifyDetails from '../../libs/getPhoneverify.json';
 
 interface SignupScreenProp extends NavigationInterface {
   testID?: string;
@@ -16,7 +16,7 @@ export default function SignupScreen(props: SignupScreenProp) {
   return (
     <Container>
       <ScrollView horizontal={true} pagingEnabled={true} ref={scrollRef}>
-        {data.map((item, ind) => (
+        {VerifyDetails.map((item, ind) => (
           <Phoneverify
             key={ind}
             index={ind}
@@ -31,17 +31,3 @@ export default function SignupScreen(props: SignupScreenProp) {
     </Container>
   );
 }
-
-const data = [
-  {
-    buttonText: 'Text me OTP',
-    context: 'country',
-    subtitle: 'Provide your phone number to receive an OTP via text message'
-  },
-  {
-    buttonText: 'Verify',
-    context: 'otp',
-    subtitle:
-      'We have sent a text message with your OTP to your phone, provide it here'
-  }
-];
