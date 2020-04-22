549db950491624acf4c1a23178850fbca6fdeb74
chidiogo-akunne <chidiogoakunne@gmail.com>
2020-04-22 03:26:52 +0100

    create email input field

--- src/screens/signup/index.tsx
+++ src/screens/signup/index.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {SafeAreaView} from 'react-native';
 
 import Button from '../../components/button';
+import Input from '../../components/input';
 import { NavigationInterface } from '../types';
 import { useThemeContext } from '../../theme';
 
@@ -13,11 +14,13 @@ interface SignupScreenProp extends NavigationInterface {
 
 export default function SignupScreen(props: SignupScreenProp) {
   const { colors } = useThemeContext();
+  const [email, setEmail] = useState("")
   return (
     <Container>
       <SafeAreaView>
       <Welcome>Welcome</Welcome>
       <Prompt>SIGN UP OR LOG IN</Prompt>
+      <Input inputLabel="Email address" placeholder="tashacombs@ymmail.com" defaultValue={email} onChangeText={input => setEmail(input)} />
       </SafeAreaView>
     </Container>
   );
