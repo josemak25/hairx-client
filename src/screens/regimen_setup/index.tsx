bbee490788bf8d074fc711a1efa8aa893b55a72a
OreAkintobi <oreakintobi@gmail.com>
2020-04-23 01:19:51 +0100

    modal closes when backdrop is pressed

--- src/screens/regimen_setup/index.tsx
+++ src/screens/regimen_setup/index.tsx
@@ -125,6 +125,9 @@ export default function RegimenSetupScreen(props: RegimenSetupScreenProp) {
         isVisible={isModalVisible}
         animationIn="slideInUp"
         animationOut="slideOutDown"
+        onBackdropPress={() => {
+          setIsModalVisible(false);
+        }}
         style={{
           display: 'flex',
           justifyContent: 'flex-end',
