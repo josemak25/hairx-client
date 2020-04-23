69e17218dca3a534abdfc37df6ee9ff662db74ea
OreAkintobi <oreakintobi@gmail.com>
2020-04-23 01:46:55 +0100

    created resumesetup component

---
+++ src/screens/regimen_setup/resume.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+import {} from './styles';
+
+interface ResumeProp {}
+
+export default function Resume(props: ResumeProp) {
+  const {} = props;
+
+  return (
+    <View>
+      <Text>Resume Screen</Text>
+    </View>
+  );
+}
