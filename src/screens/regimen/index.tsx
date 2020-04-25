330f871336117ba427f7d0a6530e2a1d11fe9fb8
OreAkintobi <oreakintobi@gmail.com>
2020-04-25 22:11:37 +0100

    built and styled home/hair goals page

--- src/screens/regimen/index.tsx
+++ src/screens/regimen/index.tsx
@@ -1,18 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavigationInterface } from '../types';
 import { StatusBar } from 'react-native';
 import { useThemeContext } from '../../theme';
 import SafeAreaView from '../../commons/safe-area-view';
 import Header from '../../commons/header';
 import Button from '../../components/button';
+import { goals } from '../../libs/regimen_hair_goals.json';
 
 import {
   Container,
   Welcome,
   HeaderTitleContainer,
   HeaderTitle,
-  HeaderTitleLabel
+  HeaderTitleLabel,
+  HairGoalsBody,
+  HairGoalsTitleContainer,
+  HairGoalsTitleText,
+  HairGoalsBodyText,
+  HairGoalsBodyContainer,
+  HairGoalsOptionsContainer,
+  HairGoalsOption,
+  HairGoalsOptionText
 } from './styles';
+import { useThemeContext } from '../../theme';
 
 interface RegimenScreenProp extends NavigationInterface {
   testID?: string;
@@ -21,6 +31,9 @@ interface RegimenScreenProp extends NavigationInterface {
 export default function RegimenScreen(props: RegimenScreenProp) {
   const { colors } = useThemeContext();
   const { navigation } = props;
+  const { colors } = useThemeContext();
+
+  const [selectedOption, setSelectedOption] = useState(null);
 
   return (
     <SafeAreaView>
@@ -37,11 +50,28 @@ export default function RegimenScreen(props: RegimenScreenProp) {
         )}
       />
       <Container>
-        <Button
-          title="Start Regimen Setup"
-          onPress={() => navigation.navigate('RegimenSetupScreen')}
-        />
-        <Welcome>Regimen Screen</Welcome>
+        <HairGoalsBody>
+          <HairGoalsTitleContainer>
+            <HairGoalsTitleText>Hair goals</HairGoalsTitleText>
+            <HairGoalsBodyContainer>
+              <HairGoalsBodyText>
+                Select your special hair goals and we’d help you set up a
+                regimen that works for your specific hair type.
+              </HairGoalsBodyText>
+            </HairGoalsBodyContainer>
+          </HairGoalsTitleContainer>
+          <HairGoalsOptionsContainer>
+            {goals.map(item => (
+              <HairGoalsOption key={item.text}>
+                <HairGoalsOptionText>{item.text}</HairGoalsOptionText>
+              </HairGoalsOption>
+            ))}
+          </HairGoalsOptionsContainer>
+          <Button
+            title="Start Regimen Setup"
+            onPress={() => navigation.navigate('RegimenSetupScreen')}
+          />
+        </HairGoalsBody>
       </Container>
     </SafeAreaView>
   );
--- src/screens/regimen/styles.ts
+++ src/screens/regimen/styles.ts
@@ -34,3 +34,60 @@ export const HeaderTitleLabel = styled.Text`
   text-transform: capitalize;
   padding: 0px 5px;
 `;
+
+export const HairGoalsBody = styled.View`
+  flex: 1;
+  justify-content: space-evenly;
+  align-items: center;
+  width: 80%;
+`;
+
+export const HairGoalsTitleContainer = styled.View`
+  height: 18%;
+  width: 100%;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const HairGoalsTitleText = styled.Text`
+  font-size: ${({ theme }) => theme.fonts.LARGE_SIZE + 15}px;
+  font-family: ${({ theme }) => theme.fonts.JOST_BOOK};
+  text-transform: uppercase;
+  text-align: center;
+`;
+
+export const HairGoalsBodyContainer = styled.View`
+  width: 85%;
+`;
+
+export const HairGoalsBodyText = styled.Text`
+  font-size: ${({ theme }) => theme.fonts.LARGE_SIZE + 5}px;
+  font-family: ${({ theme }) => theme.fonts.CORMORANT_REGULAR};
+  text-align: center;
+`;
+
+export const HairGoalsOptionsContainer = styled.View`
+  flex-direction: row;
+  flex-wrap: wrap;
+  height: 40%;
+`;
+
+export const HairGoalsOption = styled.TouchableOpacity`
+  margin-right: 10px;
+  margin-bottom: 10px;
+  height: 45px;
+  padding-left: 17px;
+  padding-right: 17px;
+  background-color: ${({ theme }) => theme.colors.INPUT_FIELD_COLOR};
+  border: 1px solid
+    ${({ theme }) => theme.colors.INACTIVE_FIELD_COLOR_LOW_OPACITY};
+  border-radius: 5px;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const HairGoalsOptionText = styled.Text`
+  font-size: ${({ theme }) => theme.fonts.LARGE_SIZE}px;
+  font-family: ${({ theme }) => theme.fonts.JOST_BOOK};
+  opacity: 0.7;
+`;
