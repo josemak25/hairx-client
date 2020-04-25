02418ba30886357780b5b5506ebbedb4da00041c
josemak25 <amakirij@gmail.com>
2020-04-25 16:35:27 +0100

    remove unsued codes and optimized question component as a scrollview on the flatlist also fixed question screen styles

--- src/screens/regimen_setup/question.tsx
+++ src/screens/regimen_setup/question.tsx
@@ -3,10 +3,10 @@ import { ScrollView } from 'react-native';
 import { useThemeContext } from '../../theme';
 import Button from '../../components/button';
 import { questions } from '../../libs/regimen_setup.json';
+import applyScale from '../../utils/applyScale';
 
 import {
   Container,
-  RegimenQuestionBody,
   QuestionContainer,
   QuestionTitle,
   QuestionRelevanceHeader,
@@ -48,64 +48,70 @@ export default function RenderItem(props: RenderItemProp) {
   } = props;
 
   return (
-    <ScrollView>
-      <Container>
-        <RegimenQuestionBody>
-          <QuestionContainer>
-            <QuestionTitle>{question}</QuestionTitle>
-            <QuestionRelevanceHeader>
-              Question Relevance
-            </QuestionRelevanceHeader>
-            <QuestionRelevanceTextContainer>
-              <QuestionRelevanceText>{questionRelevance}</QuestionRelevanceText>
-            </QuestionRelevanceTextContainer>
-          </QuestionContainer>
-          <AnswersContainer>
-            {options.map(item => (
-              <AnswerOption
-                key={item}
-                style={{
-                  backgroundColor:
-                    selectedOption === item
-                      ? colors.BG_LIGHT_GOLD_COLOR
-                      : colors.BUTTON_LIGHT_COLOR
-                }}
-                onPress={() => {
-                  setSelectedOption(item);
-                }}
-              >
-                <AnswerOptionText>{item}</AnswerOptionText>
-              </AnswerOption>
-            ))}
-          </AnswersContainer>
-          <ButtonContainer>
-            <Button
-              title="previous"
-              buttonStyle={{
-                width: 120,
-                backgroundColor: colors.BG_WHITE_COLOR,
-                borderWidth: 1,
-                borderColor: colors.BG_WHITE_COLOR
+    <ScrollView
+      showsVerticalScrollIndicator={false}
+      contentContainerStyle={{
+        flexGrow: 1,
+        paddingTop: 30,
+        paddingBottom: 50,
+        alignItems: 'center'
+      }}
+    >
+      <Container style={{ width: '90%', justifyContent: 'flex-start' }}>
+        <QuestionContainer>
+          <QuestionTitle>{question}</QuestionTitle>
+          <QuestionRelevanceHeader>Question Relevance</QuestionRelevanceHeader>
+          <QuestionRelevanceTextContainer>
+            <QuestionRelevanceText>{questionRelevance}</QuestionRelevanceText>
+          </QuestionRelevanceTextContainer>
+        </QuestionContainer>
+        <AnswersContainer>
+          {options.map(item => (
+            <AnswerOption
+              key={item}
+              style={{
+                backgroundColor:
+                  selectedOption === item
+                    ? colors.BG_LIGHT_GOLD_COLOR
+                    : colors.BUTTON_LIGHT_COLOR
               }}
-              onPress={index === firstItemIndex ? null : handlePrevious}
-              textStyle={{
-                color: colors.FONT_DARK_COLOR,
-                opacity: index === firstItemIndex ? 0.3 : 1
+              onPress={() => {
+                setSelectedOption(item);
               }}
-            />
-            <Button
-              title="next"
-              buttonStyle={{
-                width: 120,
-                backgroundColor: colors.BG_WHITE_COLOR,
-                borderWidth: 1,
-                borderColor: colors.INACTIVE_FIELD_COLOR
-              }}
-              onPress={index !== lastItemIndex ? handleNext : null}
-              textStyle={{ color: colors.FONT_DARK_COLOR }}
-            />
-          </ButtonContainer>
-        </RegimenQuestionBody>
+            >
+              <AnswerOptionText>{item}</AnswerOptionText>
+            </AnswerOption>
+          ))}
+        </AnswersContainer>
+        <ButtonContainer>
+          <Button
+            title="previous"
+            buttonStyle={{
+              width: applyScale(120),
+              backgroundColor: colors.BG_WHITE_COLOR,
+              alignItems: 'flex-start',
+              paddingLeft: 5,
+              borderWidth: 1,
+              borderColor: colors.BG_WHITE_COLOR
+            }}
+            onPress={index === firstItemIndex ? null : handlePrevious}
+            textStyle={{
+              color: colors.FONT_DARK_COLOR,
+              opacity: index === firstItemIndex ? 0.3 : 1
+            }}
+          />
+          <Button
+            title="next"
+            buttonStyle={{
+              width: applyScale(120),
+              backgroundColor: colors.BG_WHITE_COLOR,
+              borderWidth: 1,
+              borderColor: colors.INACTIVE_FIELD_COLOR
+            }}
+            onPress={index !== lastItemIndex ? handleNext : null}
+            textStyle={{ color: colors.FONT_DARK_COLOR }}
+          />
+        </ButtonContainer>
       </Container>
     </ScrollView>
   );
