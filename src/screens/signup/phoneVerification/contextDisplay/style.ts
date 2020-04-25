af812363f313f32f1805cbb97a269c14f899054a
josemak25 <amakirij@gmail.com>
2020-04-25 11:43:29 +0100

    add change to context display styles

--- src/screens/signup/phoneVerification/contextDisplay/style.ts
+++ src/screens/signup/phoneVerification/contextDisplay/style.ts
@@ -1,28 +1,28 @@
 import styled from 'styled-components/native';
-import { theme } from '../../../../theme/types';
 import applyScale from '../../../../utils/applyScale';
 
 export const Container = styled.View`
   width: 100%;
-  height: ${applyScale(45)}px;
+  height: ${applyScale(60)}px;
   margin-bottom: 30px;
   flex-direction: row;
   justify-content: center;
   align-items: center;
   border-radius: 5px;
   background-color: ${({ theme }) => theme.colors.INPUT_FIELD_COLOR};
+  padding: 0px 15px;
 `;
 
 export const Text = styled.Text`
-  opacity: 0.7;
   font-family: ${({ theme }) => theme.fonts.JOST_BOOK};
-  height: ${applyScale(18)}px;
-  font-size: ${({ theme }) => theme.fonts.LARGE_SIZE}px;
+  font-size: ${({ theme }) => theme.fonts.LARGE_SIZE + 3}px;
+  color: ${({ theme }) => theme.colors.FONT_DARK_COLOR};
+  opacity: 0.7;
 `;
 
 export const Change = styled.TouchableOpacity`
-  width: 90%;
-  height: ${applyScale(45)}px;
+  width: 100%;
+  height: 100%;
   flex-direction: row;
   justify-content: space-between;
   align-items: center;
