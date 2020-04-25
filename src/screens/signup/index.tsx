7715358be6dab6ca3de93ab43bf30f33baacf75d
josemak25 <amakirij@gmail.com>
2020-04-25 12:07:41 +0100

    add all signup screens as default export for signup navigator

--- src/screens/signup/index.tsx
+++ src/screens/signup/index.tsx
@@ -1,7 +1,13 @@
 import OTPVerificationScreen from './phoneVerification/otpVerificationScreen';
 import PhoneVerificationScreen from './phoneVerification/phoneVerificationScreen';
+import CreateAccountScreen from './create_account';
+import CreatePasswordScreen from './create_password';
+import SocialSignupScreen from './social_signup';
 
 export default {
   PhoneVerificationScreen,
-  OTPVerificationScreen
+  OTPVerificationScreen,
+  CreateAccountScreen,
+  CreatePasswordScreen,
+  SocialSignupScreen
 };
