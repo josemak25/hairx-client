e05e616e38e8b6973dd06a9cd21003f70357e513
josemak25 <amakirij@gmail.com>
2020-04-25 07:24:17 +0100

    fix custom safe area view styles

--- src/commons/header/safe-area-view/styles.ts
+++ src/commons/header/safe-area-view/styles.ts
@@ -1,12 +1,6 @@
 import styled from 'styled-components/native';
-import { Platform, StatusBar } from 'react-native';
 
 export const Container = styled.SafeAreaView`
   flex: 1;
-  align-items: center;
-  padding-top: ${Platform.select({
-    ios: 0,
-    android: StatusBar.currentHeight
-  })}px;
   background-color: ${({ theme }) => theme.colors.BG_WHITE_COLOR};
 `;
