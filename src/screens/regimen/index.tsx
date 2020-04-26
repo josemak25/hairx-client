7f72eaf92fec7b12c10f013b171e0ad67e8fa9f6
OreAkintobi <oreakintobi@gmail.com>
2020-04-26 17:19:46 +0100

    completed logic for options selection

--- src/screens/regimen/index.tsx
+++ src/screens/regimen/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { NavigationInterface } from '../types';
 import SafeAreaView from '../../commons/header/safe-area-view';
 import Header from '../../commons/header/header';
@@ -32,7 +32,8 @@ export default function RegimenScreen(props: RegimenScreenProp) {
   const { colors } = useThemeContext();
 
   const [selectedOptions, setSelectedOptions] = useState([]);
-  const [isSelected, setIsSelected] = useState(false);
+
+  useEffect(() => {}, []);
 
   return (
     <SafeAreaView>
@@ -50,7 +51,7 @@ export default function RegimenScreen(props: RegimenScreenProp) {
             <HairGoalsTitleText>Hair goals</HairGoalsTitleText>
             <HairGoalsBodyContainer>
               <HairGoalsBodyText>
-                Select your special hair goals and we’d help you set up a
+                Select your special hair goals and we’ll help you set up a
                 regimen that works for your specific hair type.
               </HairGoalsBodyText>
             </HairGoalsBodyContainer>
@@ -60,25 +61,31 @@ export default function RegimenScreen(props: RegimenScreenProp) {
               <HairGoalsOption
                 key={item.text}
                 onPress={() => {
-                  setIsSelected(true);
+                  item.isSelected = true;
+                  setSelectedOptions([...selectedOptions, item]);
                 }}
                 style={{
                   backgroundColor:
-                    isSelected === true
-                      ? item.selectedColor
+                    item.isSelected === true
+                      ? colors.BG_LIGHT_GOLD_COLOR
                       : colors.INPUT_FIELD_COLOR
                 }}
               >
                 <HairGoalsOptionText>{item.text}</HairGoalsOptionText>
-                {isSelected && (
+                {item.isSelected && (
                   <CancelOption
                     onPress={() => {
-                      setIsSelected(false);
+                      item.isSelected = false;
+                      setSelectedOptions([
+                        selectedOptions.filter(
+                          option => option.isSelected !== item.isSelected
+                        )
+                      ]);
                     }}
                   >
                     <AntDesign
                       name="close"
-                      size={10}
+                      size={12}
                       color={colors.BG_WHITE_COLOR}
                     />
                   </CancelOption>
