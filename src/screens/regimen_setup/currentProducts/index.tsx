3a60d65a9540b72a58ccd11b9846f301545d24e3
dreplica <adejo.david@aiesec.net>
2020-04-26 21:05:39 +0100

    removed all padding from button to align element correctly, moved all height and weight element to textstyle, still no definite color for add and icon for leave empty

--- src/screens/regimen_setup/currentProducts/index.tsx
+++ src/screens/regimen_setup/currentProducts/index.tsx
@@ -20,19 +20,18 @@ export default function CurrentProduct(props: Iprops) {
         <SubContent>
           <Button
             title=" &#10010; Add Product"
-            activeOpacity={1}
+            activeOpacity={0}
             buttonStyle={{
-              width: applyScale(110),
-              height: applyScale(20),
-              padding: 1,
-              opacity: 1,
-              textAlign: 'left',
-              borderWidth: 1,
-              borderColor: 'red',
+              paddingLeft: 0,
+              paddingTop: 0,
+              paddingBottom: 0,
+              paddingRigth: 0,
               backgroundColor: colors.BUTTON_LIGHT_COLOR
             }}
             textStyle={{
-              color: 'red',
+              width: applyScale(98),
+              height: applyScale(20),
+              color: '#BAB200',
               backgroundColor: colors.INPUT_FIELD_COLOR,
               fontSize: 14,
               fontFamily: fonts.JOST_MEDIUM
@@ -42,12 +41,16 @@ export default function CurrentProduct(props: Iprops) {
             title="&#8856; Leave empty"
             activeOpacity={0}
             buttonStyle={{
-              width: applyScale(120),
-              height: applyScale(20),
+              paddingLeft: 0,
+              paddingTop: 0,
+              paddingBottom: 0,
+              paddingRigth: 0,
               backgroundColor: colors.INPUT_FIELD_COLOR,
               borderWidth: 0
             }}
             textStyle={{
+              width: applyScale(90),
+              height: applyScale(20),
               color: colors.FONT_DARK_COLOR,
               opacity: 0.8,
               fontSize: 14,
