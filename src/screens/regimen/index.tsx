3df009c5543efac6456c649d1ea733d7cf34c8a3
Izuking <matthiasogbodo@gmail.com>
2020-05-01 15:12:38 +0100

    rf-add Toptab to the regimen screen

--- src/screens/regimen/index.tsx
+++ src/screens/regimen/index.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { NavigationInterface } from '../types';
-import { StatusBar } from 'react-native';
+import { StatusBar, TouchableOpacity } from 'react-native';
 import { useThemeContext } from '../../theme';
 import SafeAreaView from '../../commons/safe-area-view';
 import Header from '../../commons/header';
@@ -11,8 +11,10 @@ import {
   Welcome,
   HeaderTitleContainer,
   HeaderTitle,
-  HeaderTitleLabel
+  HeaderTitleLabel,
+  HeaderTab
 } from './styles';
+import applyScale from '../../utils/applyScale';
 
 interface RegimenScreenProp extends NavigationInterface {
   testID?: string;
@@ -21,6 +23,31 @@ interface RegimenScreenProp extends NavigationInterface {
 export default function RegimenScreen(props: RegimenScreenProp) {
   const { colors } = useThemeContext();
   const { navigation } = props;
+  const headerTabRef = useRef({
+    routineRef: null,
+    foodRef: null,
+    saloonVisitsRef: null
+  });
+
+  const handleClick = (ref: any, routePath: string) => {
+    //First set existing textDecorationLine to none
+    Object.keys(headerTabRef.current).forEach(key =>
+      headerTabRef.current[key].setNativeProps({
+        style: {
+          textDecorationLine: 'none',
+          opacity: 0.5
+        }
+      })
+    );
+
+    //Now set textDecorationLine to underline for this ref
+    ref.setNativeProps({
+      style: {
+        textDecorationLine: 'underline',
+        opacity: 1
+      }
+    });
+  };
 
   return (
     <SafeAreaView>
@@ -36,7 +63,54 @@ export default function RegimenScreen(props: RegimenScreenProp) {
           </HeaderTitleContainer>
         )}
       />
-      <SubHeaderSection navigation={navigation} />
+      <HeaderTab>
+        <TouchableOpacity
+          onPress={() => {
+            handleClick(headerTabRef.current.routineRef, '');
+          }}
+        >
+          <HeaderTitleLabel
+            ref={ref => {
+              headerTabRef.current.routineRef = ref;
+            }}
+            style={{ opacity: 0.5 }}
+          >
+            routine
+          </HeaderTitleLabel>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => {
+            handleClick(headerTabRef.current.foodRef, '');
+          }}
+        >
+          <HeaderTitleLabel
+            style={{
+              marginLeft: applyScale(30),
+              marginRight: applyScale(30),
+              opacity: 0.5
+            }}
+            ref={ref => {
+              headerTabRef.current.foodRef = ref;
+            }}
+          >
+            foods
+          </HeaderTitleLabel>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => {
+            handleClick(headerTabRef.current.saloonVisitsRef, '');
+          }}
+        >
+          <HeaderTitleLabel
+            ref={ref => {
+              headerTabRef.current.saloonVisitsRef = ref;
+            }}
+            style={{ opacity: 0.5 }}
+          >
+            saloon visits
+          </HeaderTitleLabel>
+        </TouchableOpacity>
+      </HeaderTab>
       <Container>
         <Button
           title="Start Regimen Setup"
