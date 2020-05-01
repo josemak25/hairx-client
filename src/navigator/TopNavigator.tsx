b83888450b7fb6f1afc7566d8bee2d431eb30942
dreplica <adejo.david@aiesec.net>
2020-05-01 16:26:54 +0100

    images are going into different direction as regards to their order

--- src/navigator/TopNavigator.tsx
+++ src/navigator/TopNavigator.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
-import Screens from '../screens/regimen/screens'
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import Screens from '../screens/regimen/screens';
 
 const TopNavigator = createMaterialTopTabNavigator();
 
 export default function RegimenTopNavigatot() {
-    return (<TopNavigator.Navigator initialRouteName='Routine'>
-        <TopNavigator.Screen name='Routine' component={Screens.Routine}/>
-        <TopNavigator.Screen name='Foods' component={Screens.Routine}/>
-        <TopNavigator.Screen name='Saloon visits' component={Screens.Routine}/>
+  return (
+    <TopNavigator.Navigator initialRouteName="Routine">
+      <TopNavigator.Screen name="Routine" component={Screens.Routine} />
+      <TopNavigator.Screen name="Foods" component={Screens.Routine} />
+      <TopNavigator.Screen name="Saloon visits" component={Screens.Routine} />
     </TopNavigator.Navigator>
-    );
+  );
 }
--- src/screens/regimen/RegimenScreen/cards/Card.tsx
+++ src/screens/regimen/RegimenScreen/cards/Card.tsx
@@ -1,33 +1,49 @@
-import React from 'react';
-import { Ionicons } from "@expo/vector-icons";
+import React, { useState, useEffect } from 'react';
+import { Ionicons } from '@expo/vector-icons';
+import { ImageSourcePropType } from 'react-native';
 
 import {
-    CardContainer,
-    Context,
-    Time,
-    Heading,
-    Content
-} from "./style";
+  CardContainer,
+  Context,
+  Time,
+  Image,
+  Text,
+  Heading,
+  Content
+} from './style';
 
 interface CardProps {
-    time: string;
-    order: number;
-    image: string;
-    content: string;
-    heading: string;
+  time: string;
+  order: number;
+  image: ImageSourcePropType;
+  content: string;
+  heading: string;
 }
 
 export default function Card(props: CardProps) {
-    return (
-        <CardContainer>
-            <Context>
-                <Time>
-                    <Ionicons />
-                    {props.time}
-                </Time>
-                <Heading>{props.heading}</Heading>
-                <Content>{props.content}</Content>
-            </Context>
-        </CardContainer>
-    );
+  const [state, setstate] = useState<'row' | 'row-reverse'>('row');
+
+  useEffect(() => {
+    if (props.order === 1) {
+      setstate('row-reverse')
+    }
+  }, [])
+
+
+  return (
+    <CardContainer style={{
+      flexDirection: state
+    }}>
+      <Image source={props.image} />
+
+      <Context >
+        <Time>
+          <Ionicons />
+          <Text>{props.time}</Text>
+        </Time>
+        <Heading>{props.heading}</Heading>
+        <Content>{props.content}</Content>
+      </Context>
+    </CardContainer>
+  );
 }
