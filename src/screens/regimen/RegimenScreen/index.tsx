c8b592d3f496c5ea6353126074dc4127e9c68206
dreplica <adejo.david@aiesec.net>
2020-05-01 14:51:21 +0100

    having issues using require, reverted to initial state before using require for image, removed responsive image component

--- src/screens/regimen/RegimenScreen/index.tsx
+++ src/screens/regimen/RegimenScreen/index.tsx
@@ -1,56 +1,32 @@
 import React from 'react';
+import { ScrollView } from 'react-native';
 
 import { NavigationInterface } from '../../types';
 import Card from "./cards/Card";
+import Data from "../../../libs/getRoutine.json"
 import { Container, SubTitle, Title } from "./style";
 
 interface RoutineProps extends NavigationInterface {
-    testId?: string;
+  testId?: string;
 }
 
 export default function RoutineScreen(props: RoutineProps) {
-    return (
-        <Container>
-            <SubTitle>Recommendation</SubTitle>
-            <Title>Recommendation</Title>
+  return (
+    <Container>
+      <ScrollView>
+        <SubTitle>Recommendation</SubTitle>
+        <Title>Recommendation</Title>
 
-            {
-                data.map((item, index) => <Card key={index}
-                    order={index % 2}
-                    time={item.time}
-                    heading={item.heading}
-                    image={require(item.img)}
-                    content={item.content}
-                />)
-            }
-        </Container>
-    );
+        {
+          Data.map((item, index) => <Card key={index}
+            order={index % 2}
+            time={item.time}
+            heading={item.heading}
+            image={require(item.img)}
+            content={item.content}
+          />)
+        }
+      </ScrollView>
+    </Container>
+  );
 }
-
-
-const data = [
-    {
-        img: '../../../../assets/images/routine/first.png',
-        time: 'At Night',
-        heading: 'Pre-poo your hair',
-        content: 'To stimulate  your hair follicles and grow your hair, pre-poo hair...see more'
-    },
-    {
-        img: '../../../../assets/images/routine/second.png',
-        time: 'Every Day',
-        heading: 'Use lightweight serum',
-        content: 'This is a crucial part of your daily regimen to repair damages and ... see more'
-    },
-    {
-        img: '../../../../assets/images/routine/third.png',
-        time: 'At Night',
-        heading: 'Pre-poo your hair',
-        content: 'To stimulate  your hair follicles and grow your hair, pre-poo hair...see more'
-    },
-    {
-        img: '../../../../assets/images/routine/last.png',
-        time: 'Every Day',
-        heading: 'Use lightweight serum',
-        content: 'This is a crucial part of your daily regimen to repair damages and ... see more'
-    }
-]
