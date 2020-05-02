3f5227d1c754fd8275a0e5461d322c85f035199c
josemak25 <amakirij@gmail.com>
2020-05-02 07:32:13 +0100

    refactor styles for offline bar component

--- src/commons/offline-bar/styles.ts
+++ src/commons/offline-bar/styles.ts
@@ -2,10 +2,13 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View`
   background-color: ${({ theme }) => theme.colors.BUTTON_DARK_GRAY_COLOR};
+  justify-content: center;
+  align-items: center;
 `;
 
 export const Label = styled.Text`
+  font-size: ${({ theme }) => theme.fonts.LARGE_SIZE + 2}px;
+  font-family: ${({ theme }) => theme.fonts.JOST_MEDIUM};
   color: ${({ theme }) => theme.colors.BG_WHITE_COLOR};
-  text-align: center;
   padding: 10px;
 `;
