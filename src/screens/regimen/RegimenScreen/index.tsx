e6eef07b4907eb35b1b9ec77c441de8a0cd4b802
dreplica <adejo.david@aiesec.net>
2020-05-02 10:15:27 +0100

    fixed PR changes

--- src/screens/regimen/RegimenScreen/index.tsx
+++ src/screens/regimen/RegimenScreen/index.tsx
@@ -4,17 +4,15 @@ import { ScrollView } from 'react-native-gesture-handler';
 import { NavigationInterface } from '../../types';
 import Card from './cards/Card';
 import Data from '../../../libs/getRoutine.json';
-import {
-  Container,
-  SubTitle,
-  Title
-} from './style';
+import { Container, SubTitle, Title } from './style';
 
 interface RoutineProps extends NavigationInterface {
   testId?: string;
 }
 
 export default function RoutineScreen(props: RoutineProps) {
+  const setOrder = (index: number) => (index === 1 ? 'row-reverse' : 'row');
+
   return (
     <Container>
       <ScrollView showsVerticalScrollIndicator={false}>
@@ -24,7 +22,7 @@ export default function RoutineScreen(props: RoutineProps) {
         {Data.map((item, index) => (
           <Card
             key={index}
-            order={index % 2}
+            order={setOrder(index % 2)}
             time={item.time}
             heading={item.heading}
             image={item.img}
