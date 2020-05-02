4ed3656801eb8a2d19dfe2e0544ed7f9da19373c
OreAkintobi <oreakintobi@gmail.com>
2020-05-02 14:51:13 +0100

    additional styling, removed unused imports/code

--- src/screens/food_details/index.tsx
+++ src/screens/food_details/index.tsx
@@ -1,8 +1,9 @@
-import React, { useRef, useState } from 'react';
-import { Dimensions, Text } from 'react-native';
+import React from 'react';
+import { Dimensions, ScrollView } from 'react-native';
 import { NavigationInterface } from '../types';
 import SafeAreaView from '../../commons/header/safe-area-view';
 import { useThemeContext } from '../../theme';
+import { products } from '../../libs/food_details_screen_products.json';
 import {
   Container,
   FoodImage,
@@ -12,11 +13,13 @@ import {
   FoodDetailText,
   ProductsContainer,
   ProductBox,
-  Product,
+  ProductPrice,
   ProductsHeader1,
-  ProductsHeader2
+  ProductsHeader2,
+  ProductImage,
+  ProductHeaders,
+  ProductText
 } from './styles';
-import Button from '../../components/button';
 
 const { height: SCREEN_HEIGHT } = Dimensions.get('screen');
 
@@ -31,29 +34,42 @@ export default function FoodDetailsScreen(props: FoodDetailsScreenProp) {
 
   return (
     <SafeAreaView>
-      <Container>
-        <FoodImage
-          source={require('../../../assets/images/chickpeas-in-a-bowl.png')}
-        />
-        <FoodDetailContainer>
-          <FoodDetailHeader>Cheakpeas and lentils</FoodDetailHeader>
-          <FoodDetailBenefits>Benefits</FoodDetailBenefits>
-          <FoodDetailText>
-            Chickpeas, Lentils, Nuts, seeds, Beans (great source of Protein)
-            vegans must have omega-3 and B12 supplement. Include clean Protein
-            like Hemp, Spirulina, pea Protein (add them into smoothies),
-            Berries- Loaded with antioxidants (1 Cup of strawberries contain
-            141% of your daily vitamin C needs)
-          </FoodDetailText>
-        </FoodDetailContainer>
-        <ProductsContainer>
-          <ProductBox>
-            <ProductsHeader1>RECOMMENDED</ProductsHeader1>
-            <ProductsHeader2>PRODUCTS</ProductsHeader2>
-            <Product>This is the Product</Product>
-          </ProductBox>
-        </ProductsContainer>
-      </Container>
+      <ScrollView>
+        <Container>
+          <FoodImage
+            source={require('../../../assets/images/chickpeas-in-a-bowl.png')}
+          />
+          <FoodDetailContainer>
+            <FoodDetailHeader>Cheakpeas and lentils</FoodDetailHeader>
+            <FoodDetailBenefits>Benefits</FoodDetailBenefits>
+            <FoodDetailText>
+              Chickpeas, Lentils, Nuts, seeds, Beans (great source of Protein)
+              vegans must have omega-3 and B12 supplement. Include clean Protein
+              like Hemp, Spirulina, pea Protein (add them into smoothies),
+              Berries- Loaded with antioxidants (1 Cup of strawberries contain
+              141% of your daily vitamin C needs)
+            </FoodDetailText>
+          </FoodDetailContainer>
+          <ProductsContainer>
+            <ProductHeaders>
+              <ProductsHeader1>RECOMMENDED</ProductsHeader1>
+              <ProductsHeader2>PRODUCTS</ProductsHeader2>
+            </ProductHeaders>
+            <ScrollView horizontal style={{ paddingLeft: 30 }}>
+              {products.map(product => (
+                <ProductBox key={product.name}>
+                  <ProductPrice>{product.price}</ProductPrice>
+                  <ProductImage
+                    resizeMode="contain"
+                    source={require('../../../assets/images/food_details_product_2.png')}
+                  />
+                  <ProductText>{product.name}</ProductText>
+                </ProductBox>
+              ))}
+            </ScrollView>
+          </ProductsContainer>
+        </Container>
+      </ScrollView>
     </SafeAreaView>
   );
 }
