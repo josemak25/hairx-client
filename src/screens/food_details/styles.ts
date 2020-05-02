456ea51ae3c3586d5380452bce3318884c2031f2
OreAkintobi <oreakintobi@gmail.com>
2020-05-02 14:51:13 +0100

    additional styling, removed unused imports/code

--- src/screens/food_details/styles.ts
+++ src/screens/food_details/styles.ts
@@ -2,30 +2,16 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View`
   flex: 1;
-  justify-content: space-evenly;
+  justify-content: space-between;
   align-items: center;
   background-color: ${({ theme }) => theme.colors.BG_WHITE_COLOR};
 `;
 
-export const Welcome = styled.Text`
-  font-size: ${({ theme }) => theme.fonts.LARGE_SIZE}px;
-  font-family: ${({ theme }) => theme.fonts.JOST_MEDIUM};
-  color: ${({ theme }) => theme.colors.FONT_DARK_COLOR};
-  text-transform: capitalize;
-`;
-
-export const FoodImage = styled.Image`
-  /* background-color: transparent;
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0; */
-`;
+export const FoodImage = styled.Image``;
 
 export const FoodDetailContainer = styled.View`
-  justify-content: space-evenly;
   align-items: center;
-  width: 50%;
+  width: 70%;
 `;
 
 export const FoodDetailHeader = styled.Text`
@@ -48,9 +34,10 @@ export const FoodDetailText = styled.Text`
 `;
 
 export const ProductsContainer = styled.View`
-  justify-content: space-between;
   align-items: center;
 `;
+
+export const ProductHeaders = styled.View``;
 
 export const ProductsHeader1 = styled.Text`
   font-size: ${({ theme }) => theme.fonts.LARGE_SIZE - 5}px;
@@ -67,5 +54,34 @@ export const ProductsHeader2 = styled.Text`
   text-align: center;
 `;
 
-export const ProductBox = styled.View``;
-export const Product = styled.Text``;
+export const ProductsList = styled.View`
+  flex-direction: row;
+`;
+
+export const ProductBox = styled.View`
+  background-color: ${({ theme }) => theme.colors.BUTTON_LIGHT_COLOR};
+  height: 189px;
+  margin-left: 10px;
+  margin-right: 10px;
+  width: 116px;
+  align-items: center;
+  justify-content: space-evenly;
+  border-radius: 5px;
+`;
+
+export const ProductPrice = styled.Text`
+  font-size: ${({ theme }) => theme.fonts.LARGE_SIZE - 5}px;
+  font-family: ${({ theme }) => theme.fonts.JOST_BOOK};
+  text-align: center;
+`;
+
+export const ProductImage = styled.Image`
+  width: 50%;
+  height: 50%;
+`;
+
+export const ProductText = styled.Text`
+  font-size: ${({ theme }) => theme.fonts.LARGE_SIZE - 5}px;
+  font-family: ${({ theme }) => theme.fonts.JOST_BOOK};
+  text-align: center;
+`;
