02f224b7d10f591e7d221a9261caef965c513e19
chidiogo-akunne <chidiogoakunne@gmail.com>
2020-05-04 11:31:08 +0100

    create flat list for products

---
+++ src/libs/recommendedProducts.ts
@@ -0,0 +1,26 @@
+export const RecommendedProducts = [
+  {
+    price: '$2',
+    subPrice: '74',
+    image: require('../../assets/images/bumbled_oil.png'),
+    name: 'Bumble and Bumble Invisible Oil Pre-'
+  },
+  {
+    price: '$5',
+    subPrice: '23',
+    image: require('../../assets/images/philip_kingsley_coconutoil.png'),
+    name: 'Philip Kingsley Coconut Breeze Elasticizer'
+  },
+  {
+    price: '$4',
+    subPrice: '99',
+    image: require('../../assets/images/Leonor_greyl.png'),
+    name: "Leonor Greyl L'Hu de Leonor Greyl"
+  },
+  {
+    price: '$2',
+    subPrice: '74',
+    image: require('../../assets/images/hair_lotion.png'),
+    name: 'Bumble and Bumble Invisible Oil Pre-'
+  }
+];
