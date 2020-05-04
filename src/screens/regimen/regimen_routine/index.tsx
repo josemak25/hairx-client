d066bbcdac35418321fc416730be7d13e844e0f2
chidiogo-akunne <chidiogoakunne@gmail.com>
2020-05-04 11:32:48 +0100

    create product card

--- src/screens/regimen/regimen_routine/index.tsx
+++ src/screens/regimen/regimen_routine/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { ScrollView } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 
 import { NavigationInterface } from '../../types';
@@ -7,23 +8,46 @@ import SafeAreaView from '../../../commons/safe-area-view';
 import Header from '../../../commons/header';
 import applyScale from '../../../utils/applyScale';
 import boxShadow from '../../../utils/boxShadows';
+import { RecommendedProducts } from '../../../libs/recommendedProducts';
+import Card from '../../../components/card';
+import Button from '../../../components/button';
+import { useStoreContext } from '../../../store';
 
-import { Container, Cover, IconCover, BackgroundCover, DescriptionCover, TimeCover, Night, TitleText, Description, Recommendation, Products } from './styles';
+import {
+  Container,
+  Cover,
+  IconCover,
+  BackgroundCover,
+  DescriptionCover,
+  TimeCover,
+  Night,
+  TitleText,
+  Description,
+  Recommendation,
+  Products,
+  Price,
+  PriceCover,
+  PriceSub,
+  ProductImage,
+  Name
+} from './styles';
 
 const HEADER_EXPANDED_HEIGHT = 300;
 
-
+const products = RecommendedProducts;
 
 interface RegimenRoutineScreenProp extends NavigationInterface {
   testID?: string;
 }
 
 export default function RegimenRoutineScreen(props: RegimenRoutineScreenProp) {
-  const {
-    navigation
-  } = props;
+  const { navigation } = props;
 
   const { colors } = useThemeContext();
+
+  const {
+    state: { grid }
+  } = useStoreContext();
 
   return (
     <SafeAreaView>
@@ -58,21 +82,45 @@ export default function RegimenRoutineScreen(props: RegimenRoutineScreenProp) {
         ]}
       />
       <Container>
-          <DescriptionCover>
-              <TimeCover>
-                  <Ionicons name="md-time" size={10} color={colors.FONT_BLUE_COLOR} />
-                <Night>At night</Night>
-              </TimeCover>
-              <TitleText>Pre-poo your hair</TitleText>
-              <Description>To stimulate your hair follicles and grow your hair, pre-poo your hair shampooing and give your scalp a massage with light oil along with drops of essential oils.</Description>
-          </DescriptionCover>
-          <Recommendation>
-              Recommended
-          </Recommendation>
-          <Products>Products</Products>
+        <DescriptionCover>
+          <TimeCover>
+            <Ionicons name="md-time" size={10} color={colors.FONT_BLUE_COLOR} />
+            <Night>At night</Night>
+          </TimeCover>
+          <TitleText>Pre-poo your hair</TitleText>
+          <Description>
+            To stimulate your hair follicles and grow your hair, pre-poo your
+            hair shampooing and give your scalp a massage with light oil along
+            with drops of essential oils.
+          </Description>
+        </DescriptionCover>
+        <Recommendation>Recommended</Recommendation>
+        <Products>Products</Products>
       </Container>
-
-     
+      <ScrollView
+        horizontal={true}
+        showsHorizontalScrollIndicator={false}
+        style={{ flexDirection: 'row', marginLeft: 10 }}
+      >
+        {products.map(item => (
+          <Card
+            style={{
+              backgroundColor: colors.BG_LIGHT_GRAY,
+              marginLeft: 10,
+              width: grid.cardSize / 2 + 10,
+              height: 170,
+              borderRadius: 5
+            }}
+          >
+            <PriceCover>
+              <Price>{item.price}</Price>
+              <PriceSub>{item.subPrice}</PriceSub>
+            </PriceCover>
+            <ProductImage source={item.image} />
+            <Name>{item.name}</Name>
+          </Card>
+        ))}
+      </ScrollView>
     </SafeAreaView>
   );
 }
