80a5b3cf08ceeeaff5d82bd0c1a37ffc07597cd7
chidiogo-akunne <chidiogoakunne@gmail.com>
2020-05-04 09:45:20 +0100

    style hair care routine description

--- src/screens/regimen/regimen_routine/styles.ts
+++ src/screens/regimen/regimen_routine/styles.ts
@@ -5,8 +5,8 @@ import applyScale from '../../../utils/applyScale';
 export const Container = styled.View``;
 
 export const Cover = styled.View`
-    flex: 1;
-    height: 100%
+  flex: 1;
+  height: 100%;
 `;
 
 export const IconCover = styled.TouchableOpacity`
@@ -19,6 +19,40 @@ export const IconCover = styled.TouchableOpacity`
 `;
 
 export const BackgroundCover = styled.Image`
-    flex: 1;
-    width: 100%
+  flex: 1;
+  width: 100%;
+`;
+
+export const TimeCover = styled.View`
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  margin: 4% 0 2%;
+`;
+
+export const Night = styled.Text`
+  color: ${({ theme }) => theme.colors.FONT_BLUE_COLOR};
+  font-family: ${({ theme }) => theme.fonts.JOST_BOOK};
+  font-size: ${({ theme }) => theme.fonts.SMALL_SIZE};
+  margin-left: 1%;
+  text-transform: uppercase;
+`;
+
+export const TitleText = styled.Text`
+  font-size: ${({ theme }) => theme.fonts.LARGE_SIZE + 5}px;
+  font-family: ${({ theme }) => theme.fonts.JOST_BOOK};
+  color: rgba(0, 0, 0, 0.9);
+  margin-bottom: 3%;
+`;
+
+export const Description = styled.Text`
+  text-align: center;
+  width: 65%;
+  font-family: ${({ theme }) => theme.fonts.CORMORANT_MEDIUM};
+  font-size: ${({ theme }) => theme.fonts.LARGE_SIZE};
+  color: rgba(0, 0, 0, 0.8);
+`;
+
+export const DescriptionCover = styled.View`
+  align-items: center;
 `;
