63e2c95a44042a2775dafb66207693c5bdb8bae9
dreplica <adejo.david@aiesec.net>
2020-05-05 23:18:51 +0100

    pulled from develop and created saloonvisitscreen folder

--- src/screens/regimen/RegimenScreen/index.tsx
+++ src/screens/regimen/RegimenScreen/index.tsx
@@ -11,7 +11,6 @@ interface RoutineProps extends NavigationInterface {
 }
 
 export default function RoutineScreen(props: RoutineProps) {
-
   const setOrder = (index: number) => (index === 1 ? 'row-reverse' : 'row');
   return (
     <Container>
