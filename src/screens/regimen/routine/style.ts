3b4b75533ae7a81a6787691ecdb1125a57c4c427
josemak25 <amakirij@gmail.com>
2020-05-05 19:19:27 +0100

    add regimen routine screen and regimen routine card styles

--- src/screens/regimen/routine/style.ts
+++ src/screens/regimen/routine/style.ts
@@ -1,81 +1,48 @@
 import styled from 'styled-components/native';
 import applyScale from '../../../utils/applyScale';
 
-export const Container = styled.View`
+export const Context = styled.View`
   flex: 1;
-  padding: 10px;
+  height: 100%;
+  padding: 15px 15px 0px 20px;
+  align-self: flex-end;
+`;
+
+export const ResponsiveImageContainer = styled.View`
+  width: ${applyScale(100)}px;
+  height: 100%;
+  justify-content: center;
   align-items: center;
 `;
 
-export const Title = styled.Text`
-  text-align: center;
-  margin-top: 20px;
-  margin-bottom: 40px;
-  font-family: ${({ theme: { fonts } }) => fonts.JOST_BOOK};
-  font-size: ${applyScale(30)}px;
-`;
-
-export const SubTitle = styled.Text`
-  text-align: center;
-  margin-top: 18px;
-  opacity: 0.4;
-  font-size: ${applyScale(10)}px;
-`;
-
-export const CardContainer = styled.TouchableOpacity`
-  flex: 1;
-  flex-direction: row;
-  height: ${applyScale(118)}px;
-  border-radius: 5px;
-  margin-bottom: 15px;
-  overflow: hidden;
-  background-color: ${({ theme }) => theme.colors.BG_LIGHT_BLUE_COLOR};
-`;
-
-export const Context = styled.View`
-  width: ${applyScale(252)}px;
-  justify-content: center;
-  margin-left: 19px;
-  height: ${applyScale(118)}px;
-`;
-
-export const ResponsiveImage = styled.View`
-  width: ${applyScale(93)}px;
-  height: ${applyScale(118)}px;
-`;
-
 export const Time = styled.View`
-  width: ${applyScale(60)}px;
-  height: ${applyScale(14)}px;
   flex-direction: row;
-  justify-content: flex-start;
   align-items: center;
 `;
 
 export const Text = styled.Text`
-  width: ${applyScale(43)}px;
   color: ${({ theme: { colors } }) => colors.BG_DARK_BLUE_COLOR};
-  margin-left: 5px;
-  font-size: ${applyScale(10)}px;
+  font-size: ${({ theme }) => theme.fonts.SMALL_SIZE}px;
   font-family: ${({ theme }) => theme.fonts.JOST_BOOK};
+  text-transform: uppercase;
+  padding: 0px 5px;
 `;
 
 export const Heading = styled.Text`
-  width: ${applyScale(192)}px;
-  margin-bottom: 10px;
-  font-size: ${applyScale(15)}px;
+  font-size: ${({ theme }) => theme.fonts.LARGE_SIZE}px;
   font-family: ${({ theme }) => theme.fonts.JOST_BOOK};
+  padding: 5px 0px;
 `;
 
 export const Content = styled.Text`
-  width: ${applyScale(209)}px;
-  font-size: ${applyScale(13)}px;
+  font-size: ${({ theme }) => theme.fonts.MEDIUM_SIZE + 1}px;
   font-family: ${({ theme }) => theme.fonts.CORMORANT_REGULAR};
 `;
 
 export const HeaderSection = styled.View`
   flex: 1;
   align-items: center;
+  margin-top: 18px;
 `;
 
 export const Description = styled.Text`
