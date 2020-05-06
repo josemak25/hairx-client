1ca8de2e35a406b1fe9b8f199adbb1a21abfcccb
OreAkintobi <oreakintobi@gmail.com>
2020-05-06 21:37:07 +0100

    placed style imports at bottom

--- src/screens/regimen_setup/timeSchedule/index.tsx
+++ src/screens/regimen_setup/timeSchedule/index.tsx
@@ -1,15 +1,13 @@
-import React, { useState } from 'react';
-import { useThemeContext } from '../../../theme';
+import React from 'react';
+import GotTimeIcon from '../../../../assets/icons/got_time_icon';
+import QuiteBusyIcon from '../../../../assets/icons/quite_busy_icon';
+
 import {
   AnswerOption,
   AnswerOptionText,
   AnswerOptionDetailsHeader,
   AnswerOptionDetailsText
 } from './styles';
-import boxShadow from '../../../utils/boxShadows';
-import GotTimeIcon from '../../../../assets/icons/got_time_icon';
-import QuiteBusyIcon from '../../../../assets/icons/quite_busy_icon';
-import { IconProps } from 'react-native-paper/lib/typescript/src/components/MaterialCommunityIcon';
 
 interface TimeScheduleProps {
   title: string;
