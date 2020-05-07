c6d4a818358bdb68126ff6ce05e25e74c7872c1a
chidiogo-akunne <chidiogoakunne@gmail.com>
2020-05-07 18:33:14 +0100

    create modal view

---
+++ src/screens/regimen_setup/currentProducts/addproducts/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import Modal from 'react-native-modal';
+
+import { NavigationInterface } from '../../../types';
+import { useThemeContext } from '../../../../theme';
+
+import { ModalContainer, ModalTitle } from './styles';
+
+interface SearchhProductScreenProp {
+  testID?: string;
+  isVisible: boolean;
+  onBackdropPress(): void;
+}
+
+export default function SearchProductScreen(props: SearchhProductScreenProp) {
+  const { colors, fonts } = useThemeContext();
+  return (
+    <Modal
+      isVisible={props.isVisible}
+      animationIn="slideInUp"
+      animationOut="slideOutDown"
+      onBackdropPress={props.onBackdropPress}
+      useNativeDriver={true}
+      style={{ display: 'flex', justifyContent: 'flex-end', margin: 0 }}
+    >
+      <ModalContainer>
+        <ModalTitle>Add products</ModalTitle>
+      </ModalContainer>
+    </Modal>
+  );
+}
