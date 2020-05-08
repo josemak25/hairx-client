9aeb6fb43b5d55958683d97f4ea8011cd5e33969
chidiogo-akunne <chidiogoakunne@gmail.com>
2020-05-08 09:17:35 +0100

    create search field

--- src/screens/regimen_setup/currentProducts/addproducts/index.tsx
+++ src/screens/regimen_setup/currentProducts/addproducts/index.tsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Modal from 'react-native-modal';
+import SearchInput, { createFilter } from 'react-native-search-filter';
+import { Ionicons } from '@expo/vector-icons';
 
-import { NavigationInterface } from '../../../types';
 import { useThemeContext } from '../../../../theme';
+import {productsNames} from '../../../../libs/products';
+import {brandNames} from '../../../../libs/products';
 
 import { ModalContainer, ModalTitle, ModalTitleProduct } from './styles';
 
@@ -12,8 +15,42 @@ interface SearchhProductScreenProp {
   onBackdropPress(): void;
 }
 
+const KEYS_TO_FILTERS = ['name'];
+
 export default function SearchProductScreen(props: SearchhProductScreenProp) {
-  const { colors, fonts } = useThemeContext();
+  const { colors } = useThemeContext();
+
+  const [search, setSearch] = useState({
+    searchBrand: '',
+    searchProduct: '',
+    displayBrand: false,
+    displayProduct: false,
+  });
+
+  const searchBrandUpdated = text => {
+    setSearch({
+      ...search,
+      searchBrand: text,
+      displayBrand: true
+    });
+  };
+
+  const searchProductUpdated = text => {
+    setSearch({
+      ...search,
+      searchProduct: text,
+      displayProduct: true
+    });
+  };
+
+  const filteredProducts = productsNames.filter(
+    createFilter(search.searchProduct, KEYS_TO_FILTERS)
+  );  
+
+  const filteredBrands = brandNames.filter(
+    createFilter(search.searchBrand, KEYS_TO_FILTERS)
+  );
+
   return (
     <Modal
       isVisible={props.isVisible}
@@ -24,7 +61,46 @@ export default function SearchProductScreen(props: SearchhProductScreenProp) {
       style={{ display: 'flex', justifyContent: 'flex-end', margin: 0 }}
     >
       <ModalContainer>
-        <ModalTitle>add <ModalTitleProduct>shampoo</ModalTitleProduct></ModalTitle>
+        <ModalTitle>
+          add <ModalTitleProduct>shampoo</ModalTitleProduct>
+        </ModalTitle>
+        <SearchInput
+          onChangeText={text => searchBrandUpdated(text)}
+          placeholder="Search brand name"
+          inputViewStyles={{
+            backgroundColor: colors.BG_LIGHT_GRAY,
+            width: '93%',
+            paddingTop: 20,
+            paddingBottom: 20,
+            paddingLeft: 35,
+            height: 55,
+            borderRadius: 5
+          }}
+          inputFocus={true}
+          clearIcon={<Ionicons name="ios-search" size={17} color={colors.FONT_DARK_COLOR_LOW_OPACITY} />}
+          clearIconViewStyles={{ position: 'absolute', top: 18, left: 10 }}
+        />
+        <SearchInput
+          onChangeText={text => searchProductUpdated(text)}
+          placeholder="Search product here"
+          inputViewStyles={{
+            backgroundColor: colors.BG_LIGHT_GRAY,
+            width: '93%',
+            paddingTop: 20,
+            paddingBottom: 20,
+            paddingLeft: 35,
+            height: 55,
+            borderRadius: 5,
+            marginTop: 15
+          }}
+          inputFocus={true}
+          clearIcon={<Ionicons name="ios-search" size={17} color={colors.FONT_DARK_COLOR_LOW_OPACITY} />}
+          clearIconViewStyles={{ position: 'absolute', top: 18, left: 10 }}
+        />
+        {search.displayProduct === true &&
+          filteredProducts.map(item => <ModalTitle>{item.name}</ModalTitle>)}
+          {search.displayBrand === true &&
+          filteredBrands.map(item => <ModalTitle>{item.name}</ModalTitle>)}
       </ModalContainer>
     </Modal>
   );
