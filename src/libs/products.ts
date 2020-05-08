d07db53f3613a88eae45d308d206a7e130576565
chidiogo-akunne <chidiogoakunne@gmail.com>
2020-05-08 09:15:30 +0100

    create flastlist of products and brands

---
+++ src/libs/products.ts
@@ -0,0 +1,20 @@
+export const productsNames = [
+    {
+        image: require('../../assets/images/styling_cream.png'),
+        name: 'Styling cream flexible look',
+        brand: 'nivea'
+    },
+    {
+        image: require('../../assets/images/styling_gel.png'),
+        name: 'Styling hair gel creme',
+        brand: 'nivea'
+    }
+]
+
+export const brandNames = [
+    {
+        image: require('../../assets/images/Nivea_logo.png'),
+        name: 'Nivea',
+        products: '923+ Products'
+    }
+]
