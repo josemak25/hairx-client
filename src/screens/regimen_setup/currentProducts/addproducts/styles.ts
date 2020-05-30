728b6b8bcbe293a67d560fa8fe6a8f76aa920017
chidiogo-akunne <chidiogoakunne@gmail.com>
2020-05-30 10:22:19 +0100

    style modalTitle and modalTitleProduct

--- src/screens/regimen_setup/currentProducts/addproducts/styles.ts
+++ src/screens/regimen_setup/currentProducts/addproducts/styles.ts
@@ -11,17 +11,25 @@ export const ModalContainer = styled.View`
 `;
 
 export const ModalTitle = styled.Text`
-    text-transform: capitalize;
-    margin: 5% 0;
-    font-family: ${({theme}) => theme.fonts.JOST_MEDIUM};
-    font-size: ${({theme}) => theme.fonts.LARGE_SIZE + 3}px;
-    color: ${({theme}) => theme.colors.FONT_DARK_COLOR};
+  text-transform: capitalize;
+  margin: 5% 0;
+  font-family: ${({ theme }) => theme.fonts.JOST_MEDIUM};
+  font-size: ${({ theme }) => theme.fonts.LARGE_SIZE + 3}px;
+  color: ${({ theme }) => theme.colors.FONT_DARK_COLOR};
 `;
 
 export const ModalTitleProduct = styled.Text`
-     text-transform: capitalize;
-    margin: 5% 0;
-    font-family: ${({theme}) => theme.fonts.CORMORANT_REGULAR};
-    font-size: ${({theme}) => theme.fonts.LARGE_SIZE + 3}px;
-    color: ${({theme}) => theme.colors.FONT_DARK_COLOR};
+  text-transform: capitalize;
+  margin: 5% 0;
+  font-family: ${({ theme }) => theme.fonts.CORMORANT_REGULAR};
+  font-size: ${({ theme }) => theme.fonts.LARGE_SIZE + 3}px;
+  color: ${({ theme }) => theme.colors.FONT_DARK_COLOR};
 `;
+
+export const BrandIcon = styled.Image``;
+
+export const CardView = styled.View``;
+
+export const Name = styled.Text``;
+
+export const AvailableProducts = styled.Text``;
