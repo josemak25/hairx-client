ad3012d2932b5d0bdea6447d680af57d344c89af
Izuking <matthiasogbodo@gmail.com>
2020-06-01 18:49:36 +0100

    rf-add image placeholder pre-loading

--- src/libs/responsiveImage/index.tsx
+++ src/libs/responsiveImage/index.tsx
@@ -1,7 +1,8 @@
 import React, { useState, Fragment } from 'react';
 import Animated, { Easing } from 'react-native-reanimated';
-import applyScale from '../../utils/applyScale';
+import ContentLoader from 'react-native-skeleton-content';
 
+import applyScale from '../../utils/applyScale';
 import { Image } from './styles';
 
 const ProgressiveImage = Animated.createAnimatedComponent(Image);
@@ -25,9 +26,10 @@ type ResponsiveImageProps = {
 };
 
 export default function ResponsiveImage(props: ResponsiveImageProps) {
-  const [animation] = useState({
+  const [animation, setAnimation] = useState({
     imageOpacity: new Animated.Value(0),
-    thumbnailOpacity: new Animated.Value(0)
+    thumbnailOpacity: new Animated.Value(0),
+    hideContentLoader: true
   });
 
   const width = applyScale(props.width) || 250;
@@ -57,8 +59,18 @@ export default function ResponsiveImage(props: ResponsiveImageProps) {
     if (props.onLoad) props.onLoad();
   };
 
+  const handleImageLoading = (error: any) => {
+    onLoadImage();
+    setAnimation({ ...animation, hideContentLoader: false });
+  };
+
   return (
     <Fragment>
+      <ContentLoader
+        isLoading={animation.hideContentLoader}
+        containerStyle={{ width, height }}
+        layout={[{ width, height }]}
+      />
       <ProgressiveImage
         style={[{ width, height, resizeMode }, props.style]}
         source={{ uri: thumbnailSource, cache: 'force-cache' }}
@@ -80,7 +92,7 @@ export default function ResponsiveImage(props: ResponsiveImageProps) {
         source={props.offlineImage ? props.offlineImage : onlineImage}
         onLoadStart={() => (props.onLoadStart ? props.onLoadStart() : null)}
         onProgress={() => (props.onLoadStart ? props.onLoadStart() : null)}
-        onLoad={onLoadImage}
+        onLoad={handleImageLoading}
         onError={() => (props.onError ? props.onError() : null)}
         onLoadEnd={() => (props.onLoadEnd ? props.onLoadEnd() : null)}
         testID="image-data"
